5bfe1ae40f6d1a7e36f9b4c2075fd2414b94de52
Fede Brun GDP <fedejbrun@gmail.com>
2020-08-31 15:40:04 -0300

    Arrange stuff

---
+++ misc/firstScript.py
@@ -0,0 +1,6 @@
+def main():
+    print("Bienvenidos al curso!")
+
+if __name__ == "__main__":	
+	main()
+
---
+++ misc/main.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    print("=" * 60)
+    print("Bienvenidos al curso Python para Finanzas y Ciencia de Datos")
+    print("=" * 60)
