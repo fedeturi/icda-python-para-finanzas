7432b7b2d22d283000732ba59a1ebf1639db084f
Fede Brun GDP <fedejbrun@gmail.com>
2020-08-31 15:48:36 -0300

    Delete old files

--- firstScript.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Bienvenidos al curso!")
-
-if __name__ == "__main__":	
-	main()
-
--- main.py
+++
@@ -1,4 +0,0 @@
-if __name__ == '__main__':
-    print("=" * 60)
-    print("Bienvenidos al curso Python para Finanzas y Ciencia de Datos")
-    print("=" * 60)
